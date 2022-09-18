8d96b2b20e23c4122c88a4cb675e95597e3f517c
Fangting Ma <64255604+Erica233@users.noreply.github.com>
2022-09-18 08:06:58 +0000

    add fastapi

--- dblib/querydb.py
+++ dblib/querydb.py
@@ -2,7 +2,9 @@ from databricks import sql
 import os
 
 
-def querydb(query="SELECT name, est_diameter_min, hazardous FROM neo_v2_csv ORDER BY est_diameter_min DESC LIMIT 5"):
+def querydb(
+    query="SELECT name, est_diameter_min, hazardous FROM neo_v2_csv ORDER BY est_diameter_min DESC LIMIT 5",
+):
     with sql.connect(
         server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
         http_path=os.getenv("DATABRICKS_HTTP_PATH"),
